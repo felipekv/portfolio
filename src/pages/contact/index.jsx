95ab8ae5a71e4e985566ea153c9c7fc9bc06d899
felipekv <felipekvergara@gmail.com>
2024-01-10 11:55:37 -0300

    feat: adicionado link do curriculum vitae na pagina contatos

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -3,6 +3,7 @@ import StyledContact from "./style";
 import EmailIcon from "../../assets/EmailIcon.svg";
 import WhatsappIcon from "../../assets/WhatsappIcon.svg";
 import LinkedinIcon from "../../assets/LinkedinIcon.svg";
+import CurriculumIcon from "../../assets/CurriculumIcon.svg";
 
 const Contact = () => {
     return (
@@ -36,6 +37,15 @@ const Contact = () => {
                             rel="noopener noreferrer"
                         >
                             <img src={EmailIcon} alt="Ícone de email" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://drive.google.com/file/d/1Pz_sy9Iew7lynuvM7_CoxatrYF2smfZb/view?usp=drive_link"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src={CurriculumIcon} alt="Ícone de formulário, representando o link do curriculum" />
                         </a>
                     </li>
                 </ul>
